ff59c3c62210a929002f402ed4388bf1cdbce06a
lazypwny751 <testfordnshijacking@gmail.com>
2025-06-13 19:38:07 +0300

    add multiarch, platform etc..

--- juleup.sh
+++ juleup.sh
@@ -4,16 +4,22 @@ set -e
 
 export VER="1" HELP="false"
 
-# d - set up dir.
+# $JULE_DIR/julec <- compilers goes here.
+# $JULE_DIR/jpkg  <- jpkg releases goes here.
+# $JULE_DIR/bin   <- binaries goes here. 
+# $JULE_DIR/pkg   <- global package's goes here.
 
 die() {
 	echo "${0##*/}: fatal: ${1}"
 	exit "${2:-1}"
 }
 
-while getopts ":d:r:hv" opt
+while getopts ":a:p:d:r:hvc" opt
 do
 	case "${opt}" in
+		"a")
+			export ARCH="${OPTARG}"
+		;;
 		"p")
 			export PLATFORM="${OPTARG}"
 		;;
@@ -29,13 +35,16 @@ do
 		"v")
 			export VERBOSE="$(( ${VERBOSE} + 1 ))"
 		;;
+		"c")
+			export CLEAN="true"
+		;;
 		*)
 			export COMM="${OPTARG}"
 		;;
 	esac
 done
 
-export JULE_DIR="${JULE_DIR:-~/.local/share/julelang}" RELEASE="${RELEASE:-0.1.5}"
+export JULE_DIR="${JULE_DIR:-${HOME}/.local/share/julelang}" RELEASE="${RELEASE:-0.1.5}" CLEAN="${CLEAN:-false}"
 
 shift $((OPTIND - 1))
 if [ "${#}" -gt 0 ]
@@ -51,7 +60,7 @@ case "${COPT:-help}" in
 		export NEXT="true"
 
 		# check dependencies.
-		for c in "unzip" "curl" "uname"
+		for c in "unzip" "curl" "uname" "rm" "mkdir" "cp"
 		do
 			if ! command -vV "${c}"
 			then
@@ -59,6 +68,20 @@ case "${COPT:-help}" in
 			fi
 		done
 
+		# Architecture.
+		if [ -z "${ARCH}" ]
+		then
+			case "$(uname -m)" in
+				"x86_64"|"amd64")
+					export ARCH="amd64"
+				;;
+				"aarch64")
+					export ARCH="arm64"
+				;;
+			esac
+		fi
+
+		# Platform.
 		if [ -z "${PLATFORM}" ]
 		then
 			case "$(uname)" in
@@ -68,9 +91,77 @@ case "${COPT:-help}" in
 			esac
 		fi
 
-		if "${NEXT}"
+		if "${NEXT:-false}"
 		then
-			:
+			if "${CLEAN}" && [ -d "/tmp/tmp.juleup" ]
+			then
+				rm -rf "/tmp/tmp.juleup"
+			fi
+
+			mkdir -p "/tmp/tmp.juleup"
+			echo "downloading prerelease for ${PLATFORM}-${ARCH} with release ${RELEASE}."
+
+			if [ ! -f "/tmp/tmp.juleup/${PLATFORM}-${ARCH}v${RELEASE}.zip" ]
+			then
+				curl -sSL "https://github.com/julelang/jule/releases/download/jule${RELEASE}/jule-${PLATFORM}-${ARCH}.zip" -o "/tmp/tmp.juleup/${PLATFORM}-${ARCH}v${RELEASE}.zip"
+			fi
+
+			if [ ! -f "/tmp/tmp.juleup/${PLATFORM}-${ARCH}v${RELEASE}.zip" ]
+			then
+				die "0: can't get the pre-release, are you connected to internet? is the machine name correct or architecture is true?" 
+			fi
+
+			unzip "/tmp/tmp.juleup/${PLATFORM}-${ARCH}v${RELEASE}.zip" -d "/tmp/tmp.juleup"
+
+			if [ ! -d "/tmp/tmp.juleup/jule" ]
+			then
+				die "1: can't get the pre-release, are you connected to internet? is the machine name correct or architecture is true?"
+			fi
+
+			# Set up jule's home directory.
+			echo "set up jule's home directory."
+			mkdir -p "${JULE_DIR}/julec" "${JULE_DIR}/julec/${PLATFORM}"
+
+			# check if exist's
+			if [ -d "${JULE_DIR}/julec/${PLATFORM}/julec-${ARCH}v${RELEASE}" ]
+			then
+				read -p "Do you want to replace the installation? (y/N)" opt
+				case "${opt}" in
+					[yY])
+						cp -r "/tmp/tmp.juleup/jule" "${JULE_DIR}/julec/${PLATFORM}/julec-${ARCH}v${RELEASE}"
+						echo "recopied."
+					;;
+					*)
+						echo "nothing to do, source is ready."
+					;;
+				esac
+			else
+				cp -r "/tmp/tmp.juleup/jule" "${JULE_DIR}/julec/${PLATFORM}/julec-${ARCH}v${RELEASE}"
+			fi
+
+			# This field forheck required current release of Jule compiler files.
+			# and JCD stands for JuleC Directory.
+			export JCD="${JULE_DIR}/julec/${PLATFORM}/julec-${ARCH}v${RELEASE}"
+
+			for f in "${JCD}/bin/julec" "${JCD}/std" "${JCD}/src/julec" "${JCD}/src/julec/main.jule"
+			do
+				# Superficial file integration control.
+				if [ -e "${f}" ]
+				then
+					echo "${f##*/} found.."
+				else
+					echo "${f##*/} doesn't exists!"
+					export NEXT="false"
+				fi
+			done
+
+			# Compile self hosted compiler.
+			if "${NEXT:-false}"
+			then
+				:
+			else
+				die "your installation goes wrong, please open an issue: https://github.com/lazypwny751/juleup/issues/new"
+			fi
 		else
 			die "you have unresolved packages, please install that dependencies."
 		fi
